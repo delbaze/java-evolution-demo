93d0092ae5dbbbbcc0b86539dbcb224f28b302c3
delbaze <56109313+delbaze@users.noreply.github.com>
2025-04-01 20:46:53 +0200

    Update Main.java

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -7,72 +7,41 @@ public class Main {
         System.out.println("Java version: " + System.getProperty("java.version"));
         System.out.println("Java Evolution Demo");
         System.out.println("===================");
-        
+
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
-        
+
         while (!exit) {
             System.out.println("\nChoisissez une démo à exécuter:");
-            System.out.println("1. Java 8 - Lambda et Interfaces fonctionnelles");
-            System.out.println("2. Java 8 - Streams API");
-            System.out.println("3. Java 9 - Collections Factory Methods");
-            System.out.println("4. Java 9 - Stream API Enhancements");
-            System.out.println("5. Java 9 - Private Interface Methods");
+            System.out.println("1. Java 8 - Lambda");
+            System.out.println("2. Java 8 - Interfaces fonctionnelles");
+            System.out.println("3. Java 8 - Streams API");
+            System.out.println("4. Java 9 - Collections Factory Methods");
+            System.out.println("5. Java 9 - Stream API Enhancements");
+            System.out.println("6. Java 9 - Private Interface Methods");
             System.out.println("0. Quitter");
-            
+
             System.out.print("\nVotre choix: ");
             String choice = scanner.nextLine();
-            
+
             switch (choice) {
                 case "1":
-                    System.out.println("\nDémo Java 8 - Lambda et Interfaces fonctionnelles");
-                    try {
-                        Class.forName("com.example.java8.LambdaDemo").getMethod("main", String[].class)
-                            .invoke(null, (Object) new String[]{});
-                    } catch (Exception e) {
-                        System.out.println("Cette démo n'est pas encore implémentée ou une erreur s'est produite.");
-                        e.printStackTrace();
-                    }
+                    runDemo("Démo Java 8 - Lambda", "com.example.java8.LambdaDemo");
                     break;
                 case "2":
-                    System.out.println("\nDémo Java 8 - Streams API");
-                    try {
-                        Class.forName("com.example.java8.StreamsDemo").getMethod("main", String[].class)
-                            .invoke(null, (Object) new String[]{});
-                    } catch (Exception e) {
-                        System.out.println("Cette démo n'est pas encore implémentée ou une erreur s'est produite.");
-                        e.printStackTrace();
-                    }
+                    runDemo("Démo Java 8 - Interfaces fonctionnelles", "com.example.java8.FunctionalInterfacesDemo");
                     break;
                 case "3":
-                    System.out.println("\nDémo Java 9 - Collections Factory Methods");
-                    try {
-                        Class.forName("com.example.java9.CollectionsFactoryDemo").getMethod("main", String[].class)
-                            .invoke(null, (Object) new String[]{});
-                    } catch (Exception e) {
-                        System.out.println("Cette démo n'est pas encore implémentée ou une erreur s'est produite.");
-                        e.printStackTrace();
-                    }
+                    runDemo("Démo Java 8 - Streams API", "com.example.java8.StreamsDemo");
                     break;
                 case "4":
-                    System.out.println("\nDémo Java 9 - Stream API Enhancements");
-                    try {
-                        Class.forName("com.example.java9.StreamEnhancementsDemo").getMethod("main", String[].class)
-                            .invoke(null, (Object) new String[]{});
-                    } catch (Exception e) {
-                        System.out.println("Cette démo n'est pas encore implémentée ou une erreur s'est produite.");
-                        e.printStackTrace();
-                    }
+                    runDemo("Démo Java 9 - Collections Factory Methods", "com.example.java9.CollectionsFactoryDemo");
                     break;
                 case "5":
-                    System.out.println("\nDémo Java 9 - Private Interface Methods");
-                    try {
-                        Class.forName("com.example.java9.PrivateInterfaceMethodsDemo").getMethod("main", String[].class)
-                            .invoke(null, (Object) new String[]{});
-                    } catch (Exception e) {
-                        System.out.println("Cette démo n'est pas encore implémentée ou une erreur s'est produite.");
-                        e.printStackTrace();
-                    }
+                    runDemo("Démo Java 9 - Stream API Enhancements", "com.example.java9.StreamEnhancementsDemo");
+                    break;
+                case "6":
+                    runDemo("Démo Java 9 - Private Interface Methods", "com.example.java9.PrivateInterfaceMethodsDemo");
                     break;
                 case "0":
                     exit = true;
@@ -81,8 +50,21 @@ public class Main {
                     System.out.println("Choix invalide. Veuillez réessayer.");
             }
         }
-        
+
         scanner.close();
         System.out.println("Au revoir!");
+    }
+
+    // Méthode réutilisable pour exécuter une démo
+    private static void runDemo(String demoTitle, String className) {
+        System.out.println("\n" + demoTitle);
+        try {
+            Class.forName(className)
+                .getMethod("main", String[].class)
+                .invoke(null, (Object) new String[]{});
+        } catch (Exception e) {
+            System.out.println("Cette démo n'est pas encore implémentée ou une erreur s'est produite.");
+            e.printStackTrace();
+        }
     }
 }
