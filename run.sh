31acad10a5675e50eaa999d064bdaadc28b0cc4e
delbaze <56109313+delbaze@users.noreply.github.com>
2025-04-01 18:44:56 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,6 +8,13 @@ fi
 
 VERSION=$1
 
+# Construire l'image
 podman build -f Dockerfile-java$VERSION -t java$VERSION-demo .
 
-podman run --rm -it java$VERSION-demo
+# Convertir le chemin Windows en format compatible avec Podman
+# Cette ligne gère la conversion du chemin pour qu'il fonctionne dans Git Bash sur Windows
+CURRENT_DIR=$(pwd -W 2>/dev/null || pwd | sed 's/^\///' | sed 's/\//\\/g' | sed 's/^./\0:/')
+SOURCE_PATH="/$(pwd -W 2>/dev/null || echo $CURRENT_DIR | sed 's/\\/\//g' | sed 's/://')/src"
+
+# Exécuter le conteneur avec le chemin correct
+podman run --rm -it -v "${SOURCE_PATH}:/app/src:z" java$VERSION-demo
